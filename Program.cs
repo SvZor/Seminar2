2183664e6ff0fbbec18f406cd2447d22701d3aea
SvZor <zorinasp@yandex.ru>
2024-02-27 00:49:14 +0900

    пятая задача

--- Program.cs
+++ Program.cs
@@ -87,3 +87,28 @@
 // {
 // System.Console.WriteLine("нет");
 // }
+
+
+
+// Задача 5 Напишите программу, которая принимает на вход два
+// числа и проверяет, является ли одно число квадратом другого.
+
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8,9 -> нет
+
+// System.Console.WriteLine("Введите число");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+// System.Console.WriteLine("Введите число");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if(num1% num2==0 || num2%num1==0)
+// {
+// System.Console.WriteLine("Да");
+// }
+// else
+// {
+// System.Console.WriteLine("Нет");
+// }
